0a52ef4aac0e5fb7c9a333f0eb4d150045fd99ce
John Hsu <jhsu@ets.org>
2021-04-15 10:06:44 -0400

    back jump after closing modal removed

--- preparedness/inc/js/timeline.js
+++ preparedness/inc/js/timeline.js
@@ -1,5 +1,6 @@
 $(function(){
 	var _timelineContents;
+	var _top;
 	$.getJSON( "timeline_content.json", function( data ) {
 		_timelineContents = data;
 		createEvents();
@@ -13,7 +14,15 @@ $(function(){
 			$("#pull-up-modal p.like-h4 span:nth-of-type(1)").html(_timelineContents[id].date);
 			$("#pull-up-modal p.like-h4 span:nth-of-type(2)").html(_timelineContents[id].title);
 			$("#pull-up-modal p:nth-of-type(2)").html(_timelineContents[id].body);
+			_top = $(document).scrollTop();
 			// $("article.expanded-view").hide("slide", {direction: "down"}, 2000);
+			
+		})
+
+		$("div#pull-up-modal .close-it").on("click", function() {
+			// setTimeout(function() {
+			// 	window.scrollTo(0, _top)	
+			// }, 100)
 		})
 	}
 })
